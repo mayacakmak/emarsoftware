c3f487dcda8210a08e7ddd7f80b401119456e227
Raida  Karim <raidakarim@Raidas-MacBook-Pro-2.local>
2022-01-22 19:50:54 -0800

    change unix timestamp to standard logs

--- robotbackend/scripts/belly.js
+++ robotbackend/scripts/belly.js
@@ -84,7 +84,19 @@ function Belly(robotId, scale, resizeAxis) {
     var date = new Date();
     if (target.name == 'slider') {
       Belly.bellyScreens[screenID].slider.current = target.value;
-      Belly.bellyScreens[screenID].slider.lastChanged = date.getTime();
+      var timestamp = date.getTime();
+      // Create a new JavaScript Date object based on the timestamp
+      // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+      var standardDate = new Date(timestamp);
+      // Hours part from the timestamp
+      var hours = standardDate.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + standardDate.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + standardDate.getSeconds();
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+      Belly.bellyScreens[screenID].slider.lastChanged = formattedTime;
     }
 
     if (target.name == 'checkbox') {
